31693af8091c6c200f34863a12ced82f51fa4d5a
delu-u <95361071+delu-u@users.noreply.github.com>
2024-08-04 01:35:02 +0200

    Update Main.cs

--- 2019 - C#/Main.cs
+++ 2019 - C#/Main.cs
@@ -7,7 +7,7 @@ class AoC{
     public static void Main(string[] args){
         time.Start();
         Console.WriteLine("  Day:          Part 1 / Part 2           Runtime");
-        Day1.solve(File.ReadAllLines(@"C:\Users\durkac\Desktop\AoC\2019 - C#\1"));
+        Day1.solve(File.ReadAllLines(@"1"));
         Console.WriteLine("Total:                                    " + time.Elapsed);
 
     }
